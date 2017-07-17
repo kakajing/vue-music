0484e66e99867b388dbbdedef4d79acab26e2217
jing <985812572@qq.com>
2017-07-17 23:14:17 +0800

    播放器页面基础样式、展开收起动画、歌曲播放功能的实现

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -16,9 +16,9 @@
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
-      <div class="loading-container">
+      <div v-show="!songs.length" class="loading-container">
         <loading></loading>
       </div>
     </scroll>
@@ -30,6 +30,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -75,7 +76,16 @@
       },
       back() {
         this.$router.back()
-      }
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     watch: {
       scrollY(newY) {
